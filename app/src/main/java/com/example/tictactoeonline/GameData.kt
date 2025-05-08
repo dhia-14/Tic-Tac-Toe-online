85801f7cb70eacef130de8c78bd53c9249f1ebaa
dhia-14 <ajroudidhiaeddine@gmail.com>
2025-05-08 01:51:11 +0100

    adding game model and game data files

--- app/src/main/java/com/example/tictactoeonline/GameData.kt
+++ app/src/main/java/com/example/tictactoeonline/GameData.kt
@@ -1,5 +1,14 @@
 package com.example.tictactoeonline
 
-class GameData {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
+class GameData {
+    private var _gameModel : MutableLiveData<GameModel> = MutableLiveData()
+    var gameModel : LiveData<GameModel>  = _gameModel
+
+
+    fun saveGameModel(model : GameModel){
+        _gameModel.postValue(model)
+    }
 }
